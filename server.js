1ac238e1d4c193008d41b428bbec81aba00686db
AliSina Karimi <100498538+AliSinaDevelo@users.noreply.github.com>
2023-06-08 00:47:05 +0200

    setting up a connection for the postgres database

--- server.js
+++ server.js
@@ -2,4 +2,14 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+// creating the database connection pool
+const pool = new Pool({
+	user: 'postgres',
+	host: 'localhost',
+	database: 'postgres',
+	password: 'postgres',
+	port: 5432, // Postgres server always runs on this port
+});
+
+
 
